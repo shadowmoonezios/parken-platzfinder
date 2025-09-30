57e0ba686d7a1b00028356cc7572510f2ac42a60
shadowmoonezios <agushifelde@hotmail.com>
2025-09-30 17:47:09 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Parken Platzfinder</h1>
+      <p>Finden Sie Ihren Parkplatz!</p>
+    </div>
+  );
+}
+
+export default App;
